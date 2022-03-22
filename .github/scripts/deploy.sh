6566774d43a822340bdb3610f84be9169be27e48
Saif Chaudhry <schaudhr@redhat.com>
2022-03-22 11:59:07 -0700

    feat: workflow changes

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone https://.:$TOKEN@$DEPLOY_URL build
+cd build
+git checkout $DEPLOY_BRANCH || git checkout -b $DEPLOY_BRANCH
+rm -rvf * 
+cd ..
+cp -R $WORKSPACE/dist/* ./build/
+cd build
+git config --local user.name "$GIT_USERNAME"
+git config --local user.email "$GIT_EMAIL"
+git add --all
+git commit -m "Automatically pushed build files from the $SOURCE_BRANCH branch of RedHatInsights/acs-ui"
+git push --force --quiet "https://$GIT_USERNAME:$API_TOKEN_GITHUB@$DEPLOY_URL" $DEPLOY_BRANCH:$DEPLOY_BRANCH
