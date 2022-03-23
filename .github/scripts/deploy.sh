7353f30b3dede2006e3b68d8368ed9bd0f51737d
Saif Chaudhry <schaudhr@redhat.com>
2022-03-23 11:12:31 -0700

    feat: updating workflows

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -10,5 +10,5 @@ cd build
 git config --local user.name "$GIT_USERNAME"
 git config --local user.email "$GIT_EMAIL"
 git add --all
-git commit -m "Automatically pushed build files from the $SOURCE_BRANCH branch of RedHatInsights/acs-ui"
+git commit -m "Automatically pushed build files from $SOURCE_REPO"
 git push --force --quiet "https://$GIT_USERNAME:$API_TOKEN_GITHUB@$DEPLOY_URL" $DEPLOY_BRANCH:$DEPLOY_BRANCH
