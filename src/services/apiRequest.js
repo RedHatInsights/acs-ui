f00704526581b3e7858d2dd38c14c0ac28798f57
Saif Chaudhry <schaudhr@redhat.com>
2022-08-05 11:11:30 -0700

    fix

--- src/services/apiRequest.js
+++ src/services/apiRequest.js
@@ -4,6 +4,7 @@ export const authInterceptor = (client) => {
   client.interceptors.request.use(async (cfg) => {
     await insights.chrome.auth.getUser();
     const token = await insights.chrome.auth.getToken();
+    // @TODO: Allow flexibility of using staging environment vs. prod environment
     const BASE_URL = cfg.baseURL || 'https://api.stage.openshift.com';
     const updatedCfg = { ...cfg, url: `${BASE_URL}${cfg.url}` };
     if (token) {
