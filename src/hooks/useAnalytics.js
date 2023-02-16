d6d793b270a1b595cd61851ab881a70282690e6a
Brad Rogers <braroger@redhat.com>
2023-02-16 09:48:36 -0600

    set local storage key automatically for segment

--- src/hooks/useAnalytics.js
+++ src/hooks/useAnalytics.js
@@ -2,11 +2,11 @@ import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 
 // the chrome hook assumes we're in a prod env because local development points
 // to prod.foo.redhat.com and uses the wrong write key when initializing segment.
-// to avoid this we can manually set chrome:analytics:dev to true in local storage
-// to initialize segment with the correct write key and test track events in dev
+// to avoid this we can set chrome:analytics:dev to true in local storage
+// to initialize segment with the correct write key and test events in dev
 
-// one caveat to note is we cant prevent sending the initial page/group/identity
-// events to the wrong segment environment when segment is initialized
+// one caveat to note is we cant prevent sending the initial page/group/identity events
+// to the wrong segment environment when segment is initialized before the key is set
 
 function useAnalytics() {
   const { analytics } = useChrome();
