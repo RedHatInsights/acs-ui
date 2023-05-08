68cc9ab21ed0ed829e585378ff5b4fa9d9122d40
Brad Rogers <braroger@redhat.com>
2023-05-08 16:50:20 -0500

    undo previous revert commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Reducer } from 'redux';
 
 import { Routes } from './Routes';
 import './App.scss';
+import AppContext, { defaultState } from './context/AppContext';
 
 import { getRegistry } from '@redhat-cloud-services/frontend-components-utilities/Registry';
 import NotificationsPortal from '@redhat-cloud-services/frontend-components-notifications/NotificationPortal';
@@ -11,11 +12,40 @@ import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome'
 import { Unavailable } from '@redhat-cloud-services/frontend-components/Unavailable';
 
 const App = () => {
-  const { updateDocumentTitle, getEnvironment } = useChrome();
+  const { updateDocumentTitle, getEnvironment, auth } = useChrome();
+  const [isEntitled, setIsEntitled] = useState(defaultState.isEntitled);
+  const [isEntitlementLoaded, setIsEntitlementLoaded] = useState(
+    defaultState.isEntitlementLoaded
+  );
+
+  // by default entitlement will be disabled in development, use this flag to flip it on
+  const enableEntitlementLocally = false;
+
+  const environment = getEnvironment();
+  const isDevelopment = process.env.NODE_ENV === 'development';
+  const isQAProdAuth = environment === 'qaprodauth';
 
   useEffect(() => {
-    if (process.env.NODE_ENV === 'development') {
+    const fetchEntitlements = async () => {
+      const user = await auth.getUser();
+      if (
+        user !== undefined &&
+        !isQAProdAuth &&
+        (!isDevelopment || enableEntitlementLocally)
+      ) {
+        setIsEntitled(user.entitlements.acs.is_entitled);
+        setIsEntitlementLoaded(true);
+      }
+    };
+
+    fetchEntitlements();
+
+    // because local development is pointed to prod, the useChrome hook sets isProd to true, which consequently
+    // points analytics and entitlements to prod as well. These variables are used to override those issues locally
+    if (isDevelopment || isQAProdAuth) {
       localStorage.setItem('chrome:analytics:dev', 'true');
+      setIsEntitled(true);
+      setIsEntitlementLoaded(true);
     }
 
     const registry = getRegistry();
@@ -24,15 +54,15 @@ const App = () => {
     updateDocumentTitle('acs');
   }, []);
 
-  const environment = getEnvironment();
-
   if (environment === 'stage') {
     return <Unavailable />;
   } else {
     return (
       <Fragment>
-        <NotificationsPortal />
-        <Routes />
+        <AppContext.Provider value={{ isEntitled, isEntitlementLoaded }}>
+          <NotificationsPortal />
+          <Routes />
+        </AppContext.Provider>
       </Fragment>
     );
   }
---
+++ src/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import { Navigate } from 'react-router-dom';
+import React, { useContext } from 'react';
+
+import AppContext from './context/AppContext';
+import { linkBasename, mergeToBasename } from './utils/paths';
+
+const ProtectedRoute = ({ children }: { children: React.ReactElement }) => {
+  const { isEntitled, isEntitlementLoaded } = useContext(AppContext);
+  if (!isEntitlementLoaded) {
+    return <></>;
+  } else if (isEntitled) {
+    return children;
+  } else {
+    return <Navigate to={mergeToBasename('/overview', linkBasename)} replace />;
+  }
+};
+
+export default ProtectedRoute;
