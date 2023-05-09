1dc550dfa6608ff2e5b225f1b049f44342971205
Yury Kovalev <ykovalev@redhat.com>
2023-05-09 15:31:50 +0200

    Introduce useApi hook

--- src/hooks/apis/useCreateInstance.js
+++ src/hooks/apis/useCreateInstance.js
@@ -1,17 +1,18 @@
 import { useMutation, useQueryClient } from 'react-query';
-
-import apiRequest from '../../services/apiRequest';
-
-const postInstance = async (postData) => {
-  const { data } = await apiRequest.post(
-    '/api/rhacs/v1/centrals?async=true',
-    postData
-  );
-  return data;
-};
+import useApi from './useApi';
 
 export default function useCreateInstance() {
   const queryClient = useQueryClient();
+  const apiRequest = useApi();
+
+  const postInstance = async (postData) => {
+    const { data } = await apiRequest.post(
+      '/api/rhacs/v1/centrals?async=true',
+      postData
+    );
+    return data;
+  };
+
   return useMutation(postInstance, {
     onSuccess: () => {
       // Invalidate and refetch
