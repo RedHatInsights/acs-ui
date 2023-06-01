c4db9e95475c0f92e85ebd4a5dfc17a33377cfb0
Brad Rogers <braroger@redhat.com>
2023-06-01 12:41:35 -0500

    Create temp entitlement when user subscribes but is still waiting on entitlement update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { Reducer } from 'redux';
+import { useSearchParams } from 'react-router-dom';
 
 import { Routes } from './Routes';
 import './App.scss';
@@ -11,8 +12,11 @@ import { notificationsReducer } from '@redhat-cloud-services/frontend-components
 import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
 import { Unavailable } from '@redhat-cloud-services/frontend-components/Unavailable';
 
+const LOCAL_STORAGE_ENTITLEMENT_KEY = 'acs_entitlement';
+
 const App = () => {
   const { updateDocumentTitle, getEnvironment, auth } = useChrome();
+  const [searchParams] = useSearchParams();
   const [isEntitled, setIsEntitled] = useState(defaultState.isEntitled);
   const [isEntitlementLoaded, setIsEntitlementLoaded] = useState(
     defaultState.isEntitlementLoaded
@@ -24,6 +28,27 @@ const App = () => {
   const isDevelopment = process.env.NODE_ENV === 'development';
 
   useEffect(() => {
+    const registry = getRegistry();
+    registry.register({ notifications: notificationsReducer as Reducer });
+
+    updateDocumentTitle('acs');
+  }, []);
+
+  useEffect(() => {
+    const setTemporaryLocalStorage = (
+      key: string,
+      value: string,
+      expirationTimeInHours: number
+    ) => {
+      const expirationTimestamp =
+        Date.now() + expirationTimeInHours * 60 * 60 * 1000;
+      const temporaryData = {
+        value,
+        expiresAt: expirationTimestamp,
+      };
+      localStorage.setItem(key, JSON.stringify(temporaryData));
+    };
+
     const fetchEntitlements = async () => {
       const user = await auth.getUser();
       if (user !== undefined && (!isDevelopment || enableEntitlementLocally)) {
@@ -32,21 +57,42 @@ const App = () => {
       }
     };
 
-    fetchEntitlements();
+    const handleEntitlement = () => {
+      const temporaryEntitlementString = localStorage.getItem(
+        LOCAL_STORAGE_ENTITLEMENT_KEY
+      );
+      if (temporaryEntitlementString) {
+        const temporaryEntitlementData = JSON.parse(temporaryEntitlementString);
+        if (temporaryEntitlementData.expiresAt <= Date.now()) {
+          localStorage.removeItem(LOCAL_STORAGE_ENTITLEMENT_KEY);
+          fetchEntitlements();
+        } else if (temporaryEntitlementData.value === 'true') {
+          setIsEntitled(true);
+          setIsEntitlementLoaded(true);
+        }
+      } else {
+        fetchEntitlements();
+      }
+    };
 
     // because local development is pointed to prod, the useChrome hook sets isProd to true, which consequently
     // points analytics and entitlements to prod as well. These variables are used to override those issues locally
-    if (isDevelopment) {
-      localStorage.setItem('chrome:analytics:dev', 'true');
-      setIsEntitled(true);
-      setIsEntitlementLoaded(true);
+    const handleDevelopmentEnvironment = () => {
+      if (isDevelopment) {
+        localStorage.setItem('chrome:analytics:dev', 'true');
+        setIsEntitled(true);
+        setIsEntitlementLoaded(true);
+      }
+    };
+
+    const isFromAws = searchParams.get('from-aws');
+    if (isFromAws) {
+      setTemporaryLocalStorage(LOCAL_STORAGE_ENTITLEMENT_KEY, 'true', 24);
     }
 
-    const registry = getRegistry();
-    registry.register({ notifications: notificationsReducer as Reducer });
-
-    updateDocumentTitle('acs');
-  }, []);
+    handleEntitlement();
+    handleDevelopmentEnvironment();
+  }, [searchParams]);
 
   const environment = getEnvironment();
 
